a5e33c1bb600bfbbe3380748232fc04502edfae6
HarmGoethals <38905900+HarmGoethals@users.noreply.github.com>
2018-06-11 18:42:09 +0200

    finaal

--- servo_controller.vhdl
+++ servo_controller.vhdl
@@ -23,7 +23,7 @@ port(
 end entity servo_controller;
 
 architecture behaviour of servo_controller is
-    --this is the value that decides place of servo. 
+    --this is the value that decides place of servo.
     signal data : std_logic_vector(7 downto 0) := (others=> '0');
 begin
     datacontroller : entity work.data_controller(behaviour)
